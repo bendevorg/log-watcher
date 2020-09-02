5e7da0492bbc0c6c8d5b804e00bb857bb9f8c966
unknown <guibasconti@gmail.com>
2020-09-02 20:48:12 -0300

    Add date to logs

--- utils/Log.js
+++ utils/Log.js
@@ -4,7 +4,11 @@ const logSchema = new mongoose.Schema({
   application: {
     type: String,
     required: true,
-  }
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 }, { strict: false });
 
 const LogModel = mongoose.model('log', logSchema);
